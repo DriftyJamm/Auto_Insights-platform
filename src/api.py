a167cd2e1a65dc58b0f3d310e68c7cea056cc33a
SanyamVirmani <92976385+DriftyJamm@users.noreply.github.com>
2026-05-01 22:42:03 +0530

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,12 +1,10 @@
 import requests
 
 def get_crypto_price():
-    import requests
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 
     try:
         data = requests.get(url, timeout=5).json()
-        return float(data["price"])
+        return data["bitcoin"]["usd"]
     except:
         return None
-    
