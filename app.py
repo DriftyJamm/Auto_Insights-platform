8f8c3511ba9acdfcddd90122c26adef6af6aaafb
SanyamVirmani <92976385+DriftyJamm@users.noreply.github.com>
2026-05-02 21:09:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,11 @@ elif section == "Model":
         st.markdown("<div class='card'>", unsafe_allow_html=True)
 
         try:
+            st.write("Running model...")  # DEBUG
+
             model, X = train_model(st.session_state.df)
+
+            st.write("Model trained")  # DEBUG
 
             if model is not None:
                 st.session_state.model = model
@@ -139,6 +143,24 @@ elif section == "Model":
         st.markdown("</div>", unsafe_allow_html=True)
     else:
         st.warning("Upload dataset first")
+    #st.markdown("<div class='section-title'>Model Training</div>", unsafe_allow_html=True)
+
+    #if st.session_state.df is not None:
+        #st.markdown("<div class='card'>", unsafe_allow_html=True)
+
+        #try:
+            #model, X = train_model(st.session_state.df)
+
+            #if model is not None:
+                #st.session_state.model = model
+                #st.session_state.columns = X.columns
+
+        #except Exception as e:
+            #st.error(f"Model Error: {e}")
+
+        #st.markdown("</div>", unsafe_allow_html=True)
+    #else:
+        #st.warning("Upload dataset first")
 
 # ---------------- PREDICTION ----------------
 elif section == "🔮 Prediction":
